16419cb772cfa1335a8dc99d84433b808e26204a
Alvin Mwatati <alvinmwatati@gmail.com>
2026-01-28 12:00:36 +0300

    Practiced using the subshell and variables to make a simple system info script

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#This script displays system information using system env
+
+echo "--System Info--"
+echo "Username: $USER"
+echo "Home Directory: $HOME"
+echo "List of directories: $(ls -l)"
+echo "Today is: $(date)"
